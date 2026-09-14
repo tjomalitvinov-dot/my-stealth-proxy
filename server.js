1bc25289ffe1921abf86ec9e9d90bdd44d4b6842
tjomalitvinov-dot <tjomalitvinov@gmail.com>
2026-09-14 20:15:59 +0300

    Update server.js

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ const handleParse = async (req, res) => {
     console.log(`📡 Высокоскоростной TLS-запрос текста: ${targetUrl}`);
     
     // ВСТАВЛЯЙ СЮДА СВОИ СВЕЖИЕ ПРОКСИ, КОГДА ТЕСТИРУЕШЬ
-    const proxyLine = req.query.proxy || "168.119.173.104:41346"; 
+    const proxyLine = req.query.proxy || "85.214.100.194:80"; 
     const proxyServerUrl = proxyLine.startsWith('http') ? proxyLine : `http://${proxyLine}`;
 
     try {
